08e43952aa2c116b48a3daa01b760e43e5df6311
Bryan Costanza <bryan.costanza@colorado.edu>
2019-12-23 15:47:02 -0700

    Organized main page code with subview and adjusted visual layout

--- SoMuchCoffee/MainPage.swift
+++ SoMuchCoffee/MainPage.swift
@@ -10,83 +10,60 @@ import SwiftUI
 
 struct MainPage: View {
     let menuFontSize: CGFloat = 18
-    @EnvironmentObject var settings: UserSettings
     var body: some View {
 
         VStack {
             // TITLE
+			Spacer()
             Text("So Much Coffee")
-                .foregroundColor(Color.appOrange)
+                .foregroundColor(Color("appOrange"))
                 .font(Font.custom(alfa,size: 40))
+			Spacer()
             
             // FIRST ROW
-            HStack {
-                VStack{
-                    Spacer()
-					Image("find").resizable().aspectRatio(contentMode: .fit).padding()
-                    Text("Find").font(Font.custom(lato,size: menuFontSize))
-                }
-                .frame(width: (UIScreen.main.bounds.width - 30)/3, height: UIScreen.main.bounds.height/3)
-                .border(appDarkGray, width: 1)
-                .onTapGesture {self.settings.curPage = "FIND"}
-                
-                VStack{
-                    Spacer()
-                    Image("brew").resizable().aspectRatio(contentMode: .fit)
-                    Text("Brew").font(Font.custom(lato,size: menuFontSize))
-                }
-                .frame(width: (UIScreen.main.bounds.width - 30)/3, height: UIScreen.main.bounds.height/3)
-                .border(appDarkGray, width: 1)
-				.onTapGesture {withAnimation{self.settings.curPage = "BREW"}}
-
-                VStack{
-					Spacer()
-					Image("log").resizable().aspectRatio(contentMode: .fit)
-                    Text("Log").font(Font.custom(lato,size: menuFontSize))
-                }
-                .frame(width: (UIScreen.main.bounds.width - 30)/3, height: UIScreen.main.bounds.height/3)
-                .border(appDarkGray, width: 1)
-                .onTapGesture {self.settings.curPage = "LOG"}
-            }.padding()
-                            
+			HStack (alignment: .bottom) {
+				NavItem(imageName: "find", itemName: "Find", destinationName: "FIND")
+				Divider()
+				NavItem(imageName: "brew", itemName: "Brew", destinationName: "BREW")
+				Divider()
+				NavItem(imageName: "log", itemName: "Log", destinationName: "LOG")
+			}
+			.padding([.leading, .trailing])
+			.padding(.bottom, 0)
+			
+			Divider()
+			
             //SECOND ROW
-            HStack {
-                // LEFT BUTTON
-                VStack{
-                    Spacer()
-                    Image("roasters").resizable().aspectRatio(contentMode: .fit).padding()
-                    Text("Roasters").font(Font.custom(lato,size: menuFontSize))
-                }.frame(width: (UIScreen.main.bounds.width - 30)/3, height: UIScreen.main.bounds.height/3)
-                .border(appDarkGray, width: 1)
-                .onTapGesture {self.settings.curPage = "ROASTERS"}
-                
-                                    
-                // MIDDLE BUTTON
-                VStack{
-                    Spacer()
-                    Image("origins").resizable().aspectRatio(contentMode: .fit)
-                    Text("Origins").font(Font.custom(lato,size: menuFontSize))
-                }.frame(width: (UIScreen.main.bounds.width - 30)/3, height: UIScreen.main.bounds.height/3)
-                .border(appDarkGray, width: 1)
-                .onTapGesture {self.settings.curPage = "ORIGINS"}
-                
-                                    
-                // RIGHT BUTTON
-                VStack{
-                    Spacer()
-                    Image("flavors").resizable().aspectRatio(contentMode: .fit)
-                    Text("Flavors").font(Font.custom(lato,size: menuFontSize))
-                }.frame(width: (UIScreen.main.bounds.width - 30)/3, height: UIScreen.main.bounds.height/3)
-                .border(appDarkGray, width: 1)
-                .onTapGesture {self.settings.curPage = "FLAVORS"}
+            HStack (alignment: .bottom) {
+				NavItem(imageName: "roasters", itemName: "Roasters", destinationName: "ROASTERS")
+				Divider()
+				NavItem(imageName: "origins", itemName: "Origins", destinationName: "ORIGINS")
+				Divider()
+				NavItem(imageName: "flavors", itemName: "Flavors", destinationName: "FLAVORS")
             }
+			.padding([.leading, .trailing])
+			.padding(.top, 0)
         }
     }
 }
 
 struct Main_Previews: PreviewProvider {
     static var previews: some View {
-//        MainPage().environmentObject(UserSettings())
         ContentView().environmentObject(UserSettings())
     }
+}
+
+struct NavItem: View {
+	var imageName: String
+	var itemName: String
+	var destinationName: String
+	@EnvironmentObject var settings: UserSettings
+	
+	var body: some View {
+		VStack{
+			Image(imageName).resizable().aspectRatio(contentMode: .fit).padding()
+			Text(itemName)
+		}
+		.onTapGesture {self.settings.curPage = self.destinationName}
+	}
 }

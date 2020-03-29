3ee4f3c37c845041d22434e39a5a2647bac3c8e2
Bryan Costanza <bryan.costanza@colorado.edu>
2020-03-29 11:02:44 -0600

    Create RoasterView

---
+++ SoMuchCoffee/Roasters/RoasterView.swift
@@ -0,0 +1,26 @@
+//
+//  RoasterView.swift
+//  SoMuchCoffee
+//
+//  Created by Bryan Costanza on 3/29/20.
+//  Copyright © 2020 Bryan Costanza. All rights reserved.
+//
+
+import SwiftUI
+
+struct RoasterView: View {
+    var body: some View {
+		List {
+			Section {
+				Text("Placeholder")
+			}
+		}
+		.navigationBarTitle("roaster name placeholder")
+    }
+}
+
+struct RoasterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoasterView()
+    }
+}

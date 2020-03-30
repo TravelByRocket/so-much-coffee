75e38d1159d960214d503116124a1375812dc056
Bryan Costanza <bryan.costanza@colorado.edu>
2020-03-30 14:17:42 -0600

    Put default case back because takes a long time to check that enum is exhaustive

--- Pods/FontAwesome.swift/FontAwesome/Enum.swift
+++ Pods/FontAwesome.swift/FontAwesome/Enum.swift
@@ -2836,6 +2836,7 @@ public enum FontAwesome: String {
             case .youtube: return "\u{f167}"
             case .youtubeSquare: return "\u{f431}"
             case .zhihu: return "\u{f63f}"
+            default: return ""
         }
     }
 }

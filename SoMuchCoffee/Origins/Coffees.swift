814e498892a683410b3bfdcdd2d01a39409d1a32
Bryan Costanza <bryan.costanza@colorado.edu>
2020-04-02 17:16:49 -0600

    Able to extract one entry for example data in previews

--- SoMuchCoffee/Origins/Coffees.swift
+++ SoMuchCoffee/Origins/Coffees.swift
@@ -10,9 +10,15 @@ import SwiftUI
 
 struct Coffees {
 	let items: [Coffee]
-		
+	
 	static var everyFromJSON: [Coffee] {
 		Bundle.main.decode([Coffee].self, from: "coffees.json")
+	}
+	
+	static var oneFromJSON: Coffee {
+		let count = everyFromJSON.count
+		let index = Int.random(in: 0..<count)
+		return everyFromJSON[index]
 	}
 	
 	static var all: Coffees {
